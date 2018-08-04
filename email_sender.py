ff136b3ebfe3ed010c9fa79ca19f7c08ea2dd8a6
Michael Blakesley <m.e.blakesley@gmail.com>
2018-08-04 13:52:48 -0700

    Added basic command-line arg parsing

--- email_sender.py
+++ email_sender.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
 
+import argparse
 import smtplib
 from email.message import EmailMessage
 
 import config
 
+# Parse command-line args
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', help='destination IP/host')
+parser.add_argument('-f', help='envelope "from" address')
+parser.add_argument('-t', help='envelope "to" address')
+parser.add_argument('-u', help='subject text')
+parser.add_argument('-b', help='body text')
+args = parser.parse_args()
+
+# Get vars straight
+dest_host = args.d or config.host
+subject = args.u or config.subject_text
+body = args.b or config.body_text
+env_from = args.f or config.env_from
+env_to = args.t or config.env_to
+
 # Compose email
-msg = EmailMessage()
-msg.set_content(config.body_text)
-msg['Subject'] = config.subject_text
-msg['From'] = config.env_from
-msg['To'] = config.env_to
+email = EmailMessage()
+email.set_content(body)
+email['Subject'] = subject
+email['From'] = env_from
+email['To'] = env_to
 
 # Send email
-smtp_session = smtplib.SMTP(config.host)
+smtp_session = smtplib.SMTP(dest_host)
 smtp_session.set_debuglevel(1)
-smtp_session.send_message(msg)
+smtp_session.send_message(email)
 smtp_session.quit()
 
 # Print summary
