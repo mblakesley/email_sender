af9f9233d50417b11bb87ca652f661239ad213ec
Michael Blakesley <m.e.blakesley@gmail.com>
2021-04-19 17:41:46 -0700

    Rework from/to logic; fail gracefully on 4xx/5xx

--- email_sender.py
+++ email_sender.py
@@ -5,49 +5,45 @@ import smtplib
 import string
 from email.message import EmailMessage
 from pprint import pprint
-from typing import List
+from typing import List, Optional
 
-from pydantic import BaseModel, EmailStr, FilePath, root_validator, validator
-
+from pydantic import BaseModel, EmailStr, FilePath, validator, root_validator
 
 parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
 parser.add_argument('-d', metavar='dest-host', help='destination IP/host', dest='destination')
 parser.add_argument('-p', type=int, metavar='dest-port', help='destination port', dest='port')
 parser.add_argument('-f', metavar='envelope-from', help='envelope "from" address', dest='envelope_from')
-# parser.add_argument('-F', metavar='header-from', help='header "from" address', dest='header from')
+parser.add_argument('-F', metavar='header-from', help='header "from" address', dest='header_from')
 parser.add_argument('-t', metavar='envelope-to', help='envelope "to" address', dest='envelope_to')
-# parser.add_argument('-T', metavar='header-to', help='header "to" address', dest='header to')
-parser.add_argument('-u', metavar='subject', help='subject text', dest='subject')
+parser.add_argument('-T', metavar='header-to', help='header "to" address', dest='header_to')
+parser.add_argument('-s', metavar='subject', help='subject text', dest='subject')
 parser.add_argument('-b', metavar='body', help='body text', dest='body')
-# TODO: attachments arg
+parser.add_argument('-a', metavar='attach', help='attachments (absolute path)', dest='attachments')
 # TODO: update this to match smtplib debug levels
 parser.add_argument('-v', action='store_true', help='enable verbose output', dest='verbosity')
 
 
 class EmailData(BaseModel):
+    """Validation model for email data"""
     destination: str
     port: int = 587
-    envelope_from: str = ''  # can be illegit if you want
-    header_from: str = ''
+    # avoiding EmailStr for envelope info because we want the ability to test weird vals
+    envelope_from: Optional[str] = None
+    header_from: Optional[str] = None
     # TODO: list of to's - need to come in as comma-separated strings
-    envelope_to: EmailStr
-    header_to: str = ''
+    envelope_to: Optional[str] = None
+    header_to: Optional[str] = None
     # TODO: cc/bcc (header versions? get added to to's?)
     # TODO: headers
-    subject: str = ''
-    body: str = ''
+    subject: str
+    body: str
     # TODO: properly handle '~'
     attachments: List[FilePath] = []
     verbosity: bool = False
 
-    @root_validator
-    def default_headers(cls, values: dict):
-        """Default header values to envelope values if needed"""
-        if not values.get('header_from'):
-            values['header_from'] = values['envelope_from']
-        if not values.get('header_to'):
-            values['header_to'] = values['envelope_to']
-        return values
+    @validator('attachments', pre=True)
+    def str_to_list(cls, var: str) -> List[str]:
+        return [s.strip() for s in var.split(',')]
 
     @validator('subject')
     def append_id(cls, subject: str) -> str:
@@ -56,6 +52,18 @@ class EmailData(BaseModel):
         alphanum: str = string.digits + string.ascii_letters
         ids: list = [random.choice(alphanum) for _ in range(length)]
         return '{} {}'.format(subject, ''.join(ids))
+
+    @root_validator
+    def manage_froms_tos(cls, vals: dict):
+        if vals.get("envelope_from") is None:
+            assert vals.get("header_from") is not None, \
+                'either "envelope from" or "header from" must be provided'
+            vals["envelope_from"] = vals["header_from"].split('<')[-1].strip('>')
+        if vals.get("envelope_to") is None:
+            assert vals.get("header_to") is not None, \
+                'either "envelope to" or "header to" must be provided'
+            vals["envelope_to"] = vals["header_to"].split('<')[-1].strip('>')
+        return vals
 
 
 email_data: EmailData = EmailData(**vars(parser.parse_args()))
@@ -69,10 +77,11 @@ if email_data.verbosity:
 email: EmailMessage = EmailMessage()
 email.set_content(email_data.body)
 email['Subject'] = email_data.subject
-email['From'] = email_data.header_from
-email['To'] = email_data.header_to
+if email_data.header_from is not None:
+    email['From'] = email_data.header_from
+if email_data.header_to is not None:
+    email['To'] = email_data.header_to
 
-# TODO: move to data model
 # process attachments
 for path in email_data.attachments:
     # "encoding" is only used to help determine content type
@@ -87,9 +96,13 @@ for path in email_data.attachments:
 # Send email
 smtp_session = smtplib.SMTP(host=email_data.destination, port=email_data.port)
 smtp_session.set_debuglevel(email_data.verbosity)
-smtp_session.send_message(
-    msg=email,
-    from_addr=email_data.envelope_from,
-    to_addrs=email_data.envelope_to,
-)
+try:
+    smtp_session.send_message(
+        msg=email,
+        from_addr=email_data.envelope_from,
+        to_addrs=email_data.envelope_to,
+    )
+except:
+    pass
 smtp_session.quit()
+# TODO: display error after quitting
