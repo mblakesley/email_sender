d6f04f3d064991f9be09f9134e3c827037bfa23e
Michael Blakesley <m.e.blakesley@gmail.com>
2023-02-01 16:18:13 -0800

    Remove pydantic to simplify usage

--- email_sender.py
+++ email_sender.py
@@ -1,30 +1,27 @@
-import argparse
 import mimetypes
 import random
 import smtplib
 import string
+from argparse import ArgumentParser, Namespace
 from email.message import EmailMessage
 from email.utils import make_msgid
 from pprint import pprint
-from typing import Optional
-
-from pydantic import BaseModel, EmailStr, FilePath, validator, root_validator
 
 
 def main() -> None:
-    arg_data: dict = parse_args()
-    email_data: EmailData = EmailData(**arg_data)
-    print_overview(email_data)
+    email_data: Namespace = parse_args()
+    manage_email_data(email_data)
+    print_summary(email_data)
     email: EmailMessage = compose_email_obj(email_data)
-    # email_data includes envelope data
-    send_email(email, email_data)
+    send_email(email, email_data)  # need $email_data here b/c it has envelope data
 
 
-def parse_args() -> dict:
-    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
     # TODO: mandatory args should be indicated in "-h" prompt
     parser.add_argument('-d', metavar='destination', help='destination IP/host', dest='destination')
-    parser.add_argument('-p', type=int, metavar='port', help='port', dest='port')
+    parser.add_argument('-p', type=int, default=587, metavar='port', help='port', dest='port')
     parser.add_argument('-f', metavar='envelope-from', help='envelope "from" address', dest='envelope_from')
     parser.add_argument('-F', metavar='header-from', help='header "from" address', dest='header_from')
     parser.add_argument('-t', metavar='envelope-to', help='envelope "to" addresses (comma-separated)', dest='envelope_to')
@@ -37,68 +34,52 @@ def parse_args() -> dict:
     # TODO: update this to match smtplib debug levels
     parser.add_argument('-v', action='store_true', default=False, help='enable verbose output', dest='verbosity')
 
-    return vars(parser.parse_args())
+    return parser.parse_args()
 
 
-class EmailData(BaseModel):
-    """Email data model, for clarity"""
-    destination: str
-    port: int = 587
-    # avoiding EmailStr for envelope info because we want the ability to test weird vals
-    envelope_from: Optional[str]
-    header_from: Optional[str]
-    envelope_to: list[str] = []
-    header_to: list[str] = []
-    header_cc: list[str] = []
-    bcc: list[EmailStr] = []
+def manage_email_data(email_data: Namespace) -> None:
+    """Manage & verify email input"""
+    # split CSV strings (for relevant fields)
+    for attr in ('envelope_to', 'header_to', 'header_cc', 'bcc', 'attachments'):
+        csv_str: str = getattr(email_data, attr)
+        tup_str: tuple = tuple(s.strip() for s in csv_str.split(',')) if csv_str else tuple()
+        setattr(email_data, attr, tup_str)
+
+    # process & check from's & to's
+    # smtplib can do most of this, but we do it here for output clarity
+    # also note this script treats header from/to/etc as the source of truth (put this note higher? in help output?)
+    if email_data.envelope_from is None:
+        assert email_data.header_from is not None, 'either "envelope from" or "header from" must be provided'
+        # probably want to make this optional at some point
+        email_data.envelope_from = _parse_email_str(email_data.header_from)
+    if not email_data.envelope_to:
+        assert email_data.header_to, 'either "envelope to" or "header to" must be provided'
+        hdr_rcpts: tuple = email_data.header_to + email_data.header_cc
+        email_data.envelope_to = tuple(_parse_email_str(hdr_rcpt) for hdr_rcpt in hdr_rcpts)
+
+    # append alphanum ID to subject
+    length: int = 5
+    alphanum: str = string.digits + string.ascii_letters
+    email_id: str = ''.join([random.choice(alphanum) for _ in range(length)])
+    email_data.subject += f' {email_id}'
+
     # TODO: headers
     # TODO: auto-add date headers
-    subject: str
     # TODO: html and/or plaintext content
-    body: str
-    # TODO: handle '~' when not interpolated by shell
-    attachments: list[FilePath] = []
-    verbosity: bool
-
-    @validator('envelope_to', 'header_to', 'header_cc', 'bcc', 'attachments', pre=True)
-    def str_to_list(cls, var: str) -> list[str]:
-        return [s.strip() for s in var.split(',')]
-
-    @validator('subject')
-    def append_id(cls, subject: str) -> str:
-        """Append alphanum ID to subject"""
-        length: int = 5
-        alphanum: str = string.digits + string.ascii_letters
-        ids: list = [random.choice(alphanum) for _ in range(length)]
-        return '{} {}'.format(subject, ''.join(ids))
-
-    @root_validator
-    def manage_froms_tos(cls, vals: dict):
-        # smtplib can do most of the from/to stuff, but we do it here for output clarity
-        # also note that this script treats header from/to/etc as the source of truth
-        if vals['envelope_from'] is None:
-            assert vals['header_from'] is not None, \
-                'either "envelope from" or "header from" must be provided'
-            # probably want to make this optional at some point
-            vals['envelope_from'] = cls._parse_addr(vals['header_from'])
-        if not vals['envelope_to']:
-            assert vals['header_to'], \
-                'either "envelope to" or "header to" must be provided'
-            hdr_rcpts: list[str] = vals['header_to'] + vals['header_cc']
-            vals['envelope_to'] = [cls._parse_addr(hdr_rcpt) for hdr_rcpt in hdr_rcpts]
-        return vals
-
-    @staticmethod
-    def _parse_addr(header: str) -> str:
-        return header.split('<')[-1].strip('>')
+    # TODO: for attachments, handle '~' when not interpolated by shell
 
 
-def print_overview(email_data: EmailData) -> None:
-    data_with_gaps: dict = email_data.dict(exclude={"verbosity"})
-    pprint({k: v for k, v in data_with_gaps.items() if v}, sort_dicts=False)
+def _parse_email_str(email_str: str) -> str:
+    return email_str.split('<')[-1].strip('>')
 
 
-def compose_email_obj(email_data: EmailData) -> EmailMessage:
+def print_summary(email_data: Namespace) -> None:
+    keys_to_exclude = ('verbosity',)
+    print_data: dict = {k: v for k, v in vars(email_data).items() if k not in keys_to_exclude and v is not None}
+    pprint(print_data, sort_dicts=False)
+
+
+def compose_email_obj(email_data: Namespace) -> EmailMessage:
     email: EmailMessage = EmailMessage()
     if email_data.header_from is not None:
         email['From'] = email_data.header_from
@@ -126,7 +107,7 @@ def compose_email_obj(email_data: EmailData) -> EmailMessage:
     return email
 
 
-def send_email(email: EmailMessage, email_data: EmailData) -> None:
+def send_email(email: EmailMessage, email_data: Namespace) -> None:
     smtp_session = smtplib.SMTP(host=email_data.destination, port=email_data.port)
     smtp_session.set_debuglevel(email_data.verbosity)
     if email_data.verbosity:
